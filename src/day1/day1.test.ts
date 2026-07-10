241aa214fbd95ab0a96e7c1a2663486c8c4915d8
Martin Webster <martin.webster@griffiths-waite.co.uk>
2026-07-10 13:47:48 +0100

    day 1 complete

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,17 +1,17 @@
 import "../utility/extensions";
 import { describe, expect, test } from "vitest";
 import { readTestData } from "../utility/fileHelper";
-import { exampleFunction } from "./day1";
+import { calculator, part2 } from "./day1";
 
 const dayNumber = "1";
 describe(`day ${dayNumber}`, () => {
-	test("sample", () => {
-		expect(exampleFunction("1")).toBe(1);
-	});
-
 	test("part1", () => {
 		const data = readTestData(`./src/day${dayNumber}/input.txt`);
-		const sum = data.map((line) => exampleFunction(line)).sum();
-		expect(sum).toBe(6);
+		expect(calculator(data)).toBe(430);
+	});
+
+	test("part2", () => {
+		const data = readTestData(`./src/day${dayNumber}/input.txt`);
+		expect(part2(data)).toBe(462);
 	});
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,3 +1,31 @@
-export const exampleFunction = (line: string): number | undefined => {
-    return Number(line);
-}
+export const calculator = (lines: string[]): number | undefined => {
+	var operations = lines.map((line) => {
+		if (line.startsWith("-")) {
+			return -Number(line.substring(1));
+		}
+		return Number(line.substring(1));
+	});
+	return operations.sum();
+};
+
+export const part2 = (lines: string[]): number | undefined => {
+	var operations = lines.map((line) => {
+		if (line.startsWith("-")) {
+			return -Number(line.substring(1));
+		}
+		return Number(line.substring(1));
+	});
+
+	var currentValue = 0;
+	var prevValues = new Set<number>();
+	while (true) {
+		for (const operation of operations) {
+			currentValue += operation;
+			if (prevValues.has(currentValue)) {
+				return currentValue;
+			}
+			prevValues.add(currentValue);
+		}
+	}
+	return -1;
+};
